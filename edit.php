ab8dd9724cb576ab46640fa8efa83078f245f3d5
Ionut Catalin GHEORGHIU <ionutcatalingheorghiu@pop-os.localdomain>
2020-06-02 16:19:48 +0200

    sanitized UPDATE data

--- edit.php
+++ edit.php
@@ -9,7 +9,6 @@ $query = 'SELECT
                                 Posts.user_id as postUserId, 
                                 Posts.title,
                                 Posts.body, 
-                                Posts.img,
                                 Posts.created_at,
                                 Users.id,
                                 Users.name
@@ -25,49 +24,22 @@ $post = $sth->fetch();
 
 
 
-if (isset($_POST['add-post'])) {
-
-    /* File Upload */
-    $file = $_POST['file'];
-    // var_dump($_FILES['file']);
-    $fileName = $_FILES['file']['name'];
-    $fileTmpName = $_FILES['file']['tmp_name'];
-    $fileSize = $_FILES['file']['size'];
-    $fileError = $_FILES['file']['error'];
-    $fileType = $_FILES['file']['type'];
-
-    if ($fileError  == 0) {
-        if (in_array(mime_content_type($fileTmpName), ['image/png', 'image/jpeg'])) {
-            if ($fileSize <= 3000000000) {
-
-                $urlImg = uniqid() . '.' . pathinfo($fileName, PATHINFO_EXTENSION);
-
-                unlink('uploads/' . $post['img']);
-
-                move_uploaded_file($fileTmpName, 'uploads/' . $urlImg);
-            } else {
-                echo "File to big";
-            }
-        } else {
-            echo "There was an error uploading your file";
-        }
-    } else {
-        echo "Error type file!";
-    }
-
-
+if (isset($_POST['edit-post'])) {
+    //Sanitize data
+    $title = htmlspecialchars(trim($_POST['title']));
+    $body = htmlspecialchars(trim($_POST['content']));
     //Update POST
-    $query = 'UPDATE Posts SET title = :title, body = :body, img = :img WHERE id = :id';
+    $query = '  UPDATE 
+                    Posts 
+                SET 
+                    title = :title, 
+                    body = :body
+                WHERE id = :id';
+
     $stmt = $dbh->prepare($query);
-    $stmt->bindValue(':id', $post['postId'], PDO::PARAM_STR);
-    $stmt->bindValue(':title', trim($_POST['title']), PDO::PARAM_STR);
-    $stmt->bindValue(':body', trim($_POST['content']), PDO::PARAM_STR);
-    if (!empty($urlImg)) {
-        $stmt->bindValue(':img', $urlImg, PDO::PARAM_STR);
-    } else {
-        $urlImg = $post['img'];
-        $stmt->bindValue(':img', $urlImg, PDO::PARAM_STR);
-    }
+    $stmt->bindValue(':id', $post['postId'], PDO::PARAM_INT);
+    $stmt->bindValue(':title', $title, PDO::PARAM_STR);
+    $stmt->bindValue(':body', $body, PDO::PARAM_STR);
     $stmt->execute();
 
     //Redirect
--- post.php
+++ post.php
@@ -5,22 +5,26 @@ session_start();
 
 
 //Select Post
-$query = 'SELECT   
-                                Posts.id as postId, 
-                                Posts.user_id as postUserId, 
-                                Posts.title,
-                                Posts.body, 
-                                Posts.img,
-                                Posts.created_at,
-                                Users.id,
-                                Users.name
-                                FROM Posts 
-                                INNER JOIN Users ON Users.id = Posts.user_id
-                                WHERE Posts.id=:id';
+$query = '  SELECT   
+                Posts.id as postId, 
+                Posts.user_id as postUserId, 
+                Posts.title,
+                Posts.body, 
+                Posts.created_at,
+                Users.id,
+                Users.name
+            FROM 
+                Posts 
+            INNER JOIN 
+                Users 
+            ON 
+                Users.id = Posts.user_id
+            WHERE 
+                Posts.id=:id';
 $sth = $dbh->prepare($query);
 $sth->execute([':id' =>  $_GET['post']]);
 $post = $sth->fetch();
 // }
-// var_dump($posts);
+// var_dump($post);
 
 include "post.phtml";
